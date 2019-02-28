bbc409329dea77f94c4201435dea936ad71905f5
Actireon <eheieiei37@gmail.com>
2019-02-28 14:12:55 +0200

    added-responsive-SideDrawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,7 +4,7 @@ import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 
-const toolbar = props => (
+const toolbar = () => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
         <Logo />
