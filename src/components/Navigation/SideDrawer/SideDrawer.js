1def37f0a0fcc70fbcf28a167f565213154434ea
Actireon <eheieiei37@gmail.com>
2019-02-28 14:26:28 +0200

    working-on-responsive-adjustments

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -8,7 +8,9 @@ const sideDrawer = prop => {
 
     return (
         <div className={classes.SideDrawer}>
-            <Logo />
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
             <nav>
                 <NavigationItems />
             </nav>
