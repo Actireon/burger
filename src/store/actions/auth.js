b4137f8adc51710bc7f0c659666c5e6126c2d818
Actireon <eheieiei37@gmail.com>
2019-11-29 18:56:15 +0200

    Sending an Auth to Backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +25,17 @@ export const authFail = error => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDgwWbLI0qkmcp0F0kaTX6k6-Pzmt88T4I', authData)
+            .then(res => {
+                dispatch(authSuccess(res.data));
+            })
+            .catch(err => {
+                dispatch(authFail(err));
+            })
     };
 };
