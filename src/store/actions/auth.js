a11aa81d0b35d3d672e3b73913f197da0bfb99aa
Actireon <eheieiei37@gmail.com>
2019-11-29 19:17:36 +0200

    Adding Sign-In

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = error => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -30,7 +30,12 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         };
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDgwWbLI0qkmcp0F0kaTX6k6-Pzmt88T4I', authData)
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDgwWbLI0qkmcp0F0kaTX6k6-Pzmt88T4I';
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDgwWbLI0qkmcp0F0kaTX6k6-Pzmt88T4I'
+        }
+
+        axios.post(url, authData)
             .then(res => {
                 dispatch(authSuccess(res.data));
             })
