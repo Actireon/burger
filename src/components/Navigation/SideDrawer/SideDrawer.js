ab78bac8dd7c1168ec4e29cda31be939917e4790
Actireon <eheieiei37@gmail.com>
2019-12-02 19:32:06 +0200

    upd the UI depending on auth state

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -11,13 +11,13 @@ const sideDrawer = props => {
     if (props.open) attachedClasses = [classes.SideDrawer, classes.Open];
     return (
         <Aux>
-            <Backdrop show={props.open} clicked={props.closed}/>
+            <Backdrop show={props.open} clicked={props.closed} />
             <div className={attachedClasses.join(' ')}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
                 <nav>
-                    <NavigationItems />
+                    <NavigationItems isAuthenticated={props.isAuth} />
                 </nav>
             </div>
         </Aux>
